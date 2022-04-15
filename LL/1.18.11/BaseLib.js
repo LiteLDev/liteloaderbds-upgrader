31e6ff190da9d32fdde4bf8fbd1c63cc0a05f32f
qingyu <62042544+dreamguxiang@users.noreply.github.com>
2022-04-15 09:50:07 +0800

    Fixed lF

--- LL/1.18.11/BaseLib.js
+++ LL/1.18.11/BaseLib.js
@@ -4,6 +4,7 @@ cjs.js begin, see <https://github.com/callstackexceed/cjs.js>
 
 Copyright 2021 callstackexceed
 
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
